a850f6fa0534863aceef467b23cb155a74f98a58
gregzakh <grishanz@bk.ru>
2016-05-06 20:22:04 +0600

    Create nodejs.cmd

---
+++ tests/nodejs.cmd
@@ -0,0 +1,15 @@
+0</* :
+@echo off
+  setlocal
+    set "i=0"
+    for %%i in (%*) do set /a "i+=1"
+    if %i% lss 1 echo:An argument is required.&goto:eof
+    echo:Start
+    node "%~f0" %*
+    echo:End
+  endlocal
+exit /b */0;
+
+process.argv.slice(2).forEach(function(val) {
+  console.log(val);
+});
