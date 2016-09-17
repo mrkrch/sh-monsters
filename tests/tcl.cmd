ef083a68c12cbc38805da727b14279721bdd8c9a
Grigori Zakharov <grishanz@yandex.ru>
2016-09-17 23:50:20 +0600

    Create tcl.cmd

---
+++ tests/tcl.cmd
@@ -0,0 +1,13 @@
+::set comment {
+  @echo off
+    setlocal
+      set "i=0"
+      for %%i in (%*) do set /a "i+=1"
+      if %i% lss 1 echo:An argument is required.&goto:eof
+      echo:Start
+      2>nul D:\usr\tcltk\bin\tclsh86t.exe "%~f0" %*
+      echo:End
+    endlocal
+  exit /b
+}
+foreach arg $argv { puts $arg }
