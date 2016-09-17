7180ff3fffeb66ec89fe0ff32654d1306e32bf20
Grigori Zakharov <grishanz@yandex.ru>
2016-09-17 23:49:29 +0600

    Update python.cmd

--- tests/python.cmd
+++ tests/python.cmd
@@ -6,7 +6,7 @@
     for %%i in (%*) do set /a "i+=1"
     if %i% lss 1 echo:An arguments is required.&goto:eof
     echo:Start
-    2>nul python3.4m "%~f0" %*
+    2>nul python "%~f0" %*
     echo:End
   endlocal
 exit /b
